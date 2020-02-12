42c5074500fddc194fac4bc2e11ab753f21a0ea4
AntonioGGdev <antonio.garcia-garcia@outlook.com>
2020-02-12 10:42:30 +0100

    Reorganización

---
+++ boggle/Dado.java
@@ -0,0 +1,39 @@
+package boggle;
+
+public class Dado {
+
+  char d1[] = new char[] { 'Q', 'B', 'Z', 'J', 'X', 'L' };
+  char d2[] = new char[] { 'T', 'O', 'U', 'O', 'T', 'O' };
+  char d3[] = new char[] { 'O', 'V', 'C', 'R', 'G', 'R' };
+  char d4[] = new char[] { 'A', 'A', 'A', 'F', 'S', 'R' };
+  char d5[] = new char[] { 'A', 'U', 'M', 'E', 'E', 'O' };
+
+  char d6[] = new char[] { 'E', 'H', 'L', 'R', 'D', 'O' };
+  char d7[] = new char[] { 'N', 'H', 'D', 'T', 'H', 'O' };
+  char d8[] = new char[] { 'L', 'H', 'N', 'R', 'O', 'D' };
+  char d9[] = new char[] { 'A', 'D', 'A', 'I', 'S', 'R' };
+  char d10[] = new char[] { 'U', 'I', 'F', 'A', 'S', 'R' };
+
+  char d11[] = new char[] { 'T', 'E', 'L', 'P', 'C', 'I' };
+  char d12[] = new char[] { 'S', 'S', 'N', 'S', 'E', 'U' };
+  char d13[] = new char[] { 'R', 'I', 'Y', 'P', 'R', 'H' };
+  char d14[] = new char[] { 'D', 'O', 'R', 'D', 'L', 'N' };
+  char d15[] = new char[] { 'C', 'C', 'Ñ', 'N', 'S', 'T' };
+
+  char d16[] = new char[] { 'T', 'T', 'O', 'T', 'E', 'M' };
+  char d17[] = new char[] { 'S', 'C', 'T', 'I', 'E', 'P' };
+  char d18[] = new char[] { 'E', 'A', 'N', 'D', 'N', 'N' };
+  char d19[] = new char[] { 'M', 'N', 'N', 'E', 'A', 'G' };
+  char d20[] = new char[] { 'U', 'O', 'T', 'O', 'Ñ', 'N' };
+
+  char d21[] = new char[] { 'A', 'E', 'A', 'E', 'E', 'H' };
+  char d22[] = new char[] { 'Y', 'I', 'F', 'P', 'S', 'R' };
+  char d23[] = new char[] { 'E', 'E', 'E', 'E', 'M', 'A' };
+  char d24[] = new char[] { 'I', 'T', 'A', 'T', 'I', 'E' };
+  char d25[] = new char[] { 'E', 'T', 'I', 'L', 'A', 'C' };
+
+  char caras[][] = new char[5][5];
+
+  public Dado() {}
+
+}
